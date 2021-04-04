6cb00bf1af65a41a85e17bd290f3d126823fb8d5
Artsiom Kashkou <a.kashkou@stud.elka.pw.edu.pl>
2021-04-04 21:04:16 +0300

    Fix last login date null bug

--- src/main/java/com/pochka15/funfics/converters/users/UserToUserForAdminTableDtoConverter.java
+++ src/main/java/com/pochka15/funfics/converters/users/UserToUserForAdminTableDtoConverter.java
@@ -5,17 +5,21 @@ import com.pochka15.funfics.entities.user.User;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
 public class UserToUserForAdminTableDtoConverter implements Converter<User, UserForAdmin> {
     @Override
     public UserForAdmin convert(User source) {
+        final LocalDateTime lastLoginDate = source.getActivity().getLastLoginDate();
+        final String formattedLastLoginDate = lastLoginDate == null
+                ? "" : lastLoginDate.format(DateTimeFormatter.ISO_DATE_TIME);
         return new UserForAdmin(source.getName(),
                                 source.getId(),
                                 source.isEnabled(),
                                 source.getActivity().getRegistrationDate().format(DateTimeFormatter.ISO_DATE_TIME),
-                                source.getActivity().getLastLoginDate().format(DateTimeFormatter.ISO_DATE_TIME),
+                                formattedLastLoginDate,
                                 source.getRoles());
     }
 }
